baa4deddbcf2b88ecb4f55fe02ba156a28ea778e
Fabian Poels <fabian.poels@gmail.com>
2018-02-10 23:17:03 +0100

    added some skills

--- api/data/data.js
+++ api/data/data.js
@@ -6,7 +6,52 @@ data.personal.dateOfBirth = '05/04/1987'
 data.personal.email = 'hello@fabianpoels.com'
 
 data.skills = {}
-data.skills.programmingLanguages = {}
+data.skills.programmingLanguages = [
+  {
+    language: 'Java',
+    skill: 4
+  },
+  {
+    language: 'PHP',
+    skill: 3
+  },
+  {
+    language: 'Python',
+    skill: 2
+  },
+  {
+    language: 'Perl',
+    skill: 2
+  },
+  {
+    language: 'Javascript',
+    skill: 4
+  },
+  {
+    language: 'C#',
+    skill: 2
+  }
+]
+data.skills.frameworks = [
+  {
+    name: 'Play',
+    skill: 3
+  },
+  {
+    name: 'Laravel',
+    skill: 2
+  },
+  {
+    name: 'NodeJS',
+    skill: 4
+  }
+]
+data.skills.other = [
+  {
+    name: 'Git',
+    skill: 4
+  }
+]
 
 data.experience = {}
 
